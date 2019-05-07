1477f8f242a8dfc1b373151077d07274bdcc36af
Brett Jory <bjory7@gmail.com>
2019-05-07 13:29:34 -0700

    finished assignment Ninja Class II

--- ninjaclass1.js
+++ ninjaclass1.js
@@ -4,6 +4,10 @@ function Ninja(name, health = 100) {
     var speed = 3;
     var strength = 3;
 
+    this.readStrength = function(){
+        return strength;
+    }
+
     this.sayName = function() {
         console.log(this.name);
         return this;
@@ -21,9 +25,42 @@ function Ninja(name, health = 100) {
         this.health += 10;
         return this;
     }
+
+    this.punch = function(ninja) {
+        if (ninja instanceof Ninja){
+            ninja.health -= 5;
+            console.log(ninja.name, "was punched by", this.name, "and lost 5 health!");
+        }
+        else {
+            console.log("You can only punch other ninjas!");
+            return false;
+        }
+    }
+
+    this.kick = function(ninja) {
+        if (ninja instanceof Ninja) {
+            console.log(this.strength);
+            var kickPower = this.readStrength() * 5;
+            console.log(kickPower);
+            ninja.health -= kickPower;
+            console.log(ninja.name, "was kicked by", this.name, "and lost", kickPower, "health!");
+        }
+        else {
+            console.log("You can only kick other ninjas!");
+            return false;
+        }
+    }
 }
 
-var ninja1 = new Ninja("Uesugi");
-ninja1.sayName();
-ninja1.showStats();
-ninja1.drinkSake().showStats();
+var ichika = new Ninja("Ichika");
+ichika.sayName();
+ichika.showStats();
+ichika.drinkSake().showStats();
+
+var nino = new Ninja("Nino");
+
+nino.punch(ichika);
+ichika.showStats();
+
+nino.kick(ichika);
+ichika.showStats();
