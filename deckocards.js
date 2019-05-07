7622f154aeac20382873039bc27c81a5a80aba5e
Brett Jory <bjory7@gmail.com>
2019-05-07 15:17:39 -0700

    finished assignment Deck of Cards

---
+++ deckocards.js
@@ -0,0 +1,85 @@
+class Card {
+    constructor(suit, valueString, value) {
+        this.suit = suit;
+        this.valueString = valueString;
+        this.value = value;
+    }
+
+    show(){
+        console.log(this.valueString, "of", this.suit);
+    }
+}
+
+class Deck {
+    constructor(){
+        this.cards = [];
+        var suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
+        var valueStrings = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
+        var values = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+        
+        for (let i in suits) {
+            for (let j in values) {
+                this.cards.push(new Card(suits[i], valueStrings[j], values[j]));
+            }
+        }
+    }
+
+    shuffle(){
+        var m = this.cards.length, t, i;
+
+        while (m){
+            i = Math.floor(Math.random() * m--);
+            t = this.cards[m];
+            this.cards[m] = this.cards[i];
+            this.cards[i] = t;
+        }
+        return this;
+    }
+
+    reset(){
+        var deck = new Deck;
+        this.cards = deck.cards;
+        return this;
+    }
+
+    deal() {
+        return this.cards.pop();
+    }
+}
+
+class Player {
+    constructor(name){
+        this.name = name;
+        this.hand = [];
+    }
+
+    take(deck){
+        this.hand.push(deck.deal());
+        return this;
+    }
+
+    discard(suit, valueString){
+        for (let i in this.hand) {
+            if (this.hand[i].suit == suit && this.hand[i].valueString == valueString){
+                this.hand.splice([i], 1);
+                return this;
+            }
+        }
+        console.log("You don't have that card...");
+        return this;
+    }
+}
+
+var deck = new Deck;
+console.log(deck);
+console.log(deck.cards.length);
+deck.shuffle();
+var chika = new Player("Chika");
+chika.take(deck);
+chika.take(deck);
+chika.take(deck);
+chika.take(deck);
+chika.take(deck);
+console.log(chika.hand)
+chika.discard("Diamonds", "Ten");
+console.log(chika.hand);
